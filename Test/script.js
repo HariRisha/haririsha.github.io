831e45c905ec3af2bfea1f6bdc8cdc224ee8a5d6
Hari <kocanharis55@gmail.com>
2024-09-21 17:58:58 +0400

    Final commit

--- Test/script.js
+++ Test/script.js
@@ -1,3 +1,4 @@
+//setting up variables
 let text = document.getElementById('main_text');
 let avatar = document.getElementById('avatar');
 let maxScroll = window.innerHeight / 2;
@@ -6,15 +7,16 @@ let sihyunPhoto = document.getElementById('sihyun_photo');
 let aboutSection = document.getElementById('thr_pg');
 const maxMarginLeft = 476;
 
+//detecting scroll
 window.addEventListener('scroll', () => {
     let value = window.scrollY;
 
+//calculate how much the images should move and not let them move out of the screen
     if (value <= maxScroll) {
         let scrollFactor = value / maxScroll;
         text.style.marginTop = scrollFactor * maxScroll * 2.5 + 'px';
         let calculatedMarginLeft = scrollFactor * maxScroll * 2.5;
-        avatar.style.marginLeft = Math.min(calculatedMarginLeft, maxMarginLeft) + 'px'; // Limit marginLeft to max 476px
-        console.log(avatar.style.marginLeft)
+        avatar.style.marginLeft = Math.min(calculatedMarginLeft, maxMarginLeft) + 'px'; 
         if (scrollFactor > 0) {
             avatar.src = "avatar_running.png";
         } else {
@@ -26,6 +28,8 @@ window.addEventListener('scroll', () => {
     }
 });
 
+
+//make the animation of the images on the about us page only happen when you load the page
 document.addEventListener("DOMContentLoaded", function() {
     let observer = new IntersectionObserver((entries, observer) => {
         entries.forEach(entry => {
